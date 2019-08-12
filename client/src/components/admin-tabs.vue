e8e293696f0f1011a4659a897d93b0db46f5c18f
Shanghai Wu <18620800179@163.com>
2019-08-12 20:33:16 +0800

    tabs

---
+++ client/src/components/admin-tabs.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'admin-tabs',
+  components: {},
+  data () {
+    return {}
+  },
+  computed: {},
+  methods: {}
+}
+</script>
+
+<style scoped lang="stylus">
+</style>
